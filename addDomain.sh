5135ad6a48e7e7c801baea59a5d163b34f05722c
mikeylinton <79585896+mikeylinton@users.noreply.github.com>
2021-06-10 20:48:48 +0100

    Update addDomain.sh

--- addDomain.sh
+++ addDomain.sh
@@ -11,21 +11,19 @@ domain=$1
 mkdir /var/www/$domain
 chown -R pi:www-data /var/www/$domain
 chmod -R g+s /var/www/$domain
-git clone git@github.com:mikeylinton/$domain.git /var/www/$domain
 touch /etc/apache2/sites-available/$domain.conf
 echo "<VirtualHost *:80>
         ServerName "$domain"
         ServerAlias "$domain"
         ServerAdmin webmaster@"$domain"
         DocumentRoot /var/www/"$domain"/public_html
-
         <Directory “/var/www/"$domain"/public_html”>
                 Options -Indexes +FollowSymLinks
                 AllowOverride All
         </Directory>
-
         ErrorLog \${APACHE_LOG_DIR}/"$domain"-error.log
         CustomLog \${APACHE_LOG_DIR}/"$domain"-access.log combined
 </VirtualHost>" > /etc/apache2/sites-available/$domain.conf
 a2ensite $domain
 systemctl restart apache2
+runuser -l pi -c 'git clone git@github.com:mikeylinton/mikeylinton/'$domain'.git /var/www/'$domain
