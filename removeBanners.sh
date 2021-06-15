a6846904ed81036f56ae662cdcfe7f0f7e734327
mikeylinton <79585896+mikeylinton@users.noreply.github.com>
2021-06-15 09:40:39 +0100

    Create removeBanners.sh

---
+++ removeBanners.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+if [[ $(/usr/bin/id -u) -ne 0 ]]; then
+        echo "Not running as root"
+        exit
+fi
+printf "\nServerTokens Prod\nServerSignature  Off\n" >> /etc/apache2/conf-enabled/security.conf
+cd /etc/php/7.3/apache2/
+find . -name 'php.ini' -exec sed -i -e 's/expose_php = Off/expose_php = On/g' {} \;
+sytemctl restart apache2.service
