4996c22812edb0ee32d3bfcc98a285e21fb9fa62
mikeylinton <79585896+mikeylinton@users.noreply.github.com>
2021-06-15 21:19:31 +0100

    Update fail2banSetup.sh

--- fail2banSetup.sh
+++ fail2banSetup.sh
@@ -10,8 +10,10 @@ find . -name 'jail.local' -exec sed -i -e 's/logpath\s*=\s*%(apache_error_log)s/
 find . -name 'jail.local' -exec sed -i -e 's/logpath\s*=\s*%(apache_access_log)s//g' {} \;
 find . -name 'jail.local' -exec sed -i -e 's/logpath\s*=\s*%(nginx_access_log)s//g' {} \;
 find . -name 'jail.local' -exec sed -i -e 's/\s*%(apache_access_log)s//g' {} \;
-#apache-404 jail
-find . -name 'jail.local' -exec sed -i -e 's/\[apache-auth\]/\[apache-404\]\nenabled = true\nport = http,https\nfilter = apache-404\nlogpath = \/var\/log\/fail2ban\/apache-404.log\nbantime = 3600\nfindtime = 600\nmaxretry = 5\n\n\[apache-auth\]/g' {} \;
+#apache-forbidden jail
+find . -name 'jail.local' -exec sed -i -e 's/\[apache-auth\]/\[apache-forbidden\]\nenabled = true\nport = http,https\nfilter = apache-forbidden\nlogpath = \/var\/log\/fail2ban\/apache-forbidden.log\nbantime = 3600\nfindtime = 600\nmaxretry = 5\n\n\[apache-auth\]/g' {} \;
+#apache-unavailable jail
+find . -name 'jail.local' -exec sed -i -e 's/\[apache-auth\]/\[apache-unavailable\]\nenabled = true\nport = http,https\nfilter = apache-unavailable\nlogpath = \/var\/log\/fail2ban\/apache-unavailable.log\nbantime = 3600\nfindtime = 600\nmaxretry = 5\n\n\[apache-auth\]/g' {} \;
 #apache-auth jail
 find . -name 'jail.local' -exec sed -i -e 's/\[apache-auth\]/\[apache-auth\]\nenabled  = true\nfilter   = apache-auth\nlogpath = \/var\/log\/fail2ban\/apache-auth.log/g' {} \;
 #apache-badbots jail
@@ -26,11 +28,15 @@ find . -name 'jail.local' -exec sed -i -e 's/\[apache-overflows\]/\[apache-overf
 find . -name 'jail.local' -exec sed -i -e 's/\[php-url-fopen\]/\[php-url-fopen\]\nenabled  = true\nlogpath = \/var\/log\/fail2ban\/php-url-fopen.log/g' {} \;
 mkdir /var/log/fail2ban
 cd /var/log/fail2ban/
-touch apache-404.log
+touch apache-forbidden.log
+touch apache-unavailable.log
 touch apache-auth.log
 touch apache-badbots.log
 touch apache-nohome.log
 touch apache-noscript.log
 touch apache-overflows.log
 touch php-url-fopen.log
+cd /etc/fail2ban/filter.d/
+printf "[Definition]\nfailregex = <HOST> - - .*HTTP/[0-9]+(.[0-9]+)?\" 403 *" > apache-forbidden.conf
+printf "[Definition]\nfailregex = <HOST> - - .*HTTP/[0-9]+(.[0-9]+)?\" 503 *\n<HOST> - - .*HTTP/[0-9]+(.[0-9]+)?\" 500 *\n<HOST> - - .*HTTP/[0-9]+(.[0-9]+)?\" 404 451 *" > apache-unavailable.conf
 systemctl restart fail2ban
