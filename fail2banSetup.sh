f331ae7b5ecb615cc5fd42333263d45a22e977ba
mikeylinton <79585896+mikeylinton@users.noreply.github.com>
2021-06-15 21:41:19 +0100

    Update fail2banSetup.sh

--- fail2banSetup.sh
+++ fail2banSetup.sh
@@ -11,9 +11,11 @@ find . -name 'jail.local' -exec sed -i -e 's/logpath\s*=\s*%(apache_access_log)s
 find . -name 'jail.local' -exec sed -i -e 's/logpath\s*=\s*%(nginx_access_log)s//g' {} \;
 find . -name 'jail.local' -exec sed -i -e 's/\s*%(apache_access_log)s//g' {} \;
 #apache-forbidden jail
-find . -name 'jail.local' -exec sed -i -e 's/\[apache-auth\]/\[apache-forbidden\]\nenabled = true\nport = http,https\nfilter = apache-forbidden\nlogpath = \/var\/log\/fail2ban\/apache-forbidden.log\nbantime = 3600\nfindtime = 600\nmaxretry = 5\n\n\[apache-auth\]/g' {} \;
+#
+#find . -name 'jail.local' -exec sed -i -e 's/\[apache-auth\]/\[apache-forbidden\]\nenabled = true\nport = http,https\nfilter = apache-forbidden\nlogpath = \/var\/log\/fail2ban\/apache-forbidden.log\nbantime = 3600\nfindtime = 600\nmaxretry = 5\n\n\[apache-auth\]/g' {} \;
 #apache-unavailable jail
-find . -name 'jail.local' -exec sed -i -e 's/\[apache-auth\]/\[apache-unavailable\]\nenabled = true\nport = http,https\nfilter = apache-unavailable\nlogpath = \/var\/log\/fail2ban\/apache-unavailable.log\nbantime = 3600\nfindtime = 600\nmaxretry = 5\n\n\[apache-auth\]/g' {} \;
+#find . -name 'jail.local' -exec sed -i -e 's/\[apache-auth\]/\[apache-unavailable\]\nenabled = true\nport = http,https\nfilter = apache-unavailable\nlogpath = \/var\/log\/fail2ban\/apache-unavailable.log\nbantime = 3600\nfindtime = 600\nmaxretry = 5\n\n\[apache-auth\]/g' {} \;
+#
 #apache-auth jail
 find . -name 'jail.local' -exec sed -i -e 's/\[apache-auth\]/\[apache-auth\]\nenabled  = true\nfilter   = apache-auth\nlogpath = \/var\/log\/fail2ban\/apache-auth.log/g' {} \;
 #apache-badbots jail
@@ -36,8 +38,7 @@ touch apache-nohome.log
 touch apache-noscript.log
 touch apache-overflows.log
 touch php-url-fopen.log
-cd /etc/fail2ban/filter.d/
-printf "[Definition]\nfailregex = ^<HOST> - - .*HTTP/[0-9]+(.[0-9]+)?\" 403 *" > apache-forbidden.conf
-printf "[Definition]\nfailregex = ^<HOST> - - .*HTTP/[0-9]+(.[0-9]+)?\" 503 *\n^<HOST> - - .*HTTP/[0-9]+(.[0-9]+)?\" 500 *\n^<HOST> - - .*HTTP/[0-9]+(.[0-9]+)?\" 404 451 *" > apache-unavailable.conf
+#cd /etc/fail2ban/filter.d/
+#printf "[Definition]\nfailregex = ^<HOST> - - .*HTTP/[0-9]+(.[0-9]+)?\" 403 *" > apache-forbidden.conf
+#printf "[Definition]\nfailregex = ^<HOST> - - .*HTTP/[0-9]+(.[0-9]+)?\" 503 *\n^<HOST> - - .*HTTP/[0-9]+(.[0-9]+)?\" 500 *\n^<HOST> - - .*HTTP/[0-9]+(.[0-9]+)?\" 404 451 *" > apache-unavailable.conf
 systemctl restart fail2ban
-fail2ban-client status
