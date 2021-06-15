75c1eff0eb954733cd7af1b6598e36888efb52bd
mikeylinton <79585896+mikeylinton@users.noreply.github.com>
2021-06-15 09:45:44 +0100

    Update removeBanners.sh

--- removeBanners.sh
+++ removeBanners.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [[ $(/usr/bin/id -u) -ne 0 ]]; then
-        echo "Not running as root"
+        echo "This script must be run as root"
         exit
 fi
 printf "\nServerTokens Prod\nServerSignature  Off\n" >> /etc/apache2/conf-enabled/security.conf
