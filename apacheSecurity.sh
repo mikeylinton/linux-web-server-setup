fa5ff88c12a010cba30bc3371ad24d5472cc15a6
mikeylinton <79585896+mikeylinton@users.noreply.github.com>
2021-06-16 10:56:07 +0100

    Update and rename removeBanners.sh to apacheSecurity.sh

--- apacheSecurity.sh
+++ apacheSecurity.sh
@@ -4,6 +4,7 @@ if [[ $(/usr/bin/id -u) -ne 0 ]]; then
         exit
 fi
 printf "\nServerTokens Prod\nServerSignature  Off\n" >> /etc/apache2/conf-enabled/security.conf
+printf "<Directory /var/www/>\n\tAllowOverride None\n\tRequire all denied\n</Directory>\n" >> /etc/apache2/conf-enabled/security.conf
 cd /etc/php/7.3/apache2/
 find . -name 'php.ini' -exec sed -i -e 's/expose_php = Off/expose_php = On/g' {} \;
 sytemctl restart apache2.service
