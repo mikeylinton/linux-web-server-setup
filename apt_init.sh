3e001dd8f285146d9f449a543870bc536fe006ee
mikeylinton <79585896+mikeylinton@users.noreply.github.com>
2021-08-08 10:42:33 +0100

    Update apt_init.sh

--- apt_init.sh
+++ apt_init.sh
@@ -2,4 +2,6 @@ if [[ $(/usr/bin/id -u) -ne 0 ]]; then
     echo "This script must be run as root"
     exit
 fi
-apt install apache2 php libapache2-mod-php -y
+apt update
+apt upgrade -y
+apt install apache2 php libapache2-mod-php ufw fail2ban -y
