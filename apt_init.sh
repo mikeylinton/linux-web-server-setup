76ef8fe99c420779c2be348d94da5e24ca54e4c7
mikeylinton <79585896+mikeylinton@users.noreply.github.com>
2021-08-09 16:16:31 +0100

    Update apt_init.sh

--- apt_init.sh
+++ apt_init.sh
@@ -4,4 +4,4 @@ if [[ $(/usr/bin/id -u) -ne 0 ]]; then
 fi
 apt update
 apt upgrade -y
-apt install apache2 php libapache2-mod-php ufw fail2ban -y
+apt install apache2 php libapache2-mod-php ufw fail2ban neovim -y
