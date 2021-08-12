a8975ceb41d173d8b2976dda8294229946f4dcf6
mikeylinton <79585896+mikeylinton@users.noreply.github.com>
2021-08-12 16:36:37 +0100

    Update addDomain.sh

--- addDomain.sh
+++ addDomain.sh
@@ -3,29 +3,42 @@ if [[ $(/usr/bin/id -u) -ne 0 ]]; then
         echo "This script must be run as root"
         exit
 fi
-if [ "$1" = "" ] ; then
-        echo "Usage: ./"$0" DomainName"
+if [ "$2" = "" ] ; then
+        echo "Usage: ./"$0" DomainName SubDomain"
         exit
 fi
 domain=$1
+subDomain=$2
 mkdir /var/www/$domain
 chown -R pi:www-data /var/www/$domain
 touch /etc/apache2/sites-available/$domain.conf
+
 echo "<VirtualHost *:80>
         ServerName "$domain"
-        ServerAlias "$domain"
+        ServerAlias www."$domain"
         ServerAdmin webmaster@"$domain"
-        DocumentRoot /var/www/"$domain"/public_html
-        Alias /favicon.ico /var/www/"$domain"/public_html/required/favicon.ico
-        <Directory “/var/www/"$domain"/public_html”>
+        DocumentRoot /var/www/"$domain"/www/public_html
+        ErrorLog \${APACHE_LOG_DIR}/"$domain"-error.log
+        CustomLog \${APACHE_LOG_DIR}/"$domain"-access.log combined
+        Redirect permanent / http://www."$domain"/
+</VirtualHost>" > /etc/apache2/sites-available/$domain.conf
+
+echo "<VirtualHost *:80>
+        ServerName "$domain"
+        ServerAlias "$subDomain"."$domain"
+        ServerAdmin webmaster@"$domain"
+        DocumentRoot /var/www/"$domain"/"$subDomain"/public_html
+        <Directory “/var/www/"$domain"/"$subDomain"/public_html”>
                 Options -Indexes +FollowSymLinks
                 AllowOverride All
         </Directory>
-        ErrorLog \${APACHE_LOG_DIR}/"$domain"-error.log
-        CustomLog \${APACHE_LOG_DIR}/"$domain"-access.log combined
-</VirtualHost>" > /etc/apache2/sites-available/$domain.conf
-printf "<Directory /var/www/"$domain"/public_html>\n\tAllowOverride All\n\tRequire all granted\n</Directory>\n" >> /etc/apache2/conf-enabled/security.conf
-printf "<Directory /var/www/"$domain"/public_html/required>\n\tOptions -Indexes\n</Directory>\n" >> /etc/apache2/conf-enabled/security.conf
+        ErrorLog \${APACHE_LOG_DIR}/"$subDomain"."$domain"-error.log
+        CustomLog \${APACHE_LOG_DIR}/"$subDomain"."$domain"-access.log combined
+</VirtualHost>" > /etc/apache2/sites-available/"$subDomain".$domain.conf
+printf "<Directory /var/www/"$domain">\n\tOptions -Indexes\n</Direct
+ory>\n" >> /etc/apache2/conf-enabled/security.conf
+printf "<Directory /var/www/"$domain"/"$subDomain"/public_html>\n\tAllowOverride All\n\tRequire all granted\n</Directory>\n" >> /etc/apache2/conf-enabled/security.conf
+printf "<Directory /var/www/"$domain"/"$subDomain"/public_html/required>\n\tOptions -Indexes\n</Directory>\n" >> /etc/apache2/conf-enabled/security.conf
 a2ensite $domain
-systemctl restart apache2
-#runuser -l pi -c 'eval `ssh-agent -s` && ssh-add /home/pi/.ssh/github.id_ed25519 && git clone git@github.com:mikeylinton/'$domain'.git /var/www/'$domain
+a2ensite $subDomain.$domain
+service restart apache2
