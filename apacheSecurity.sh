b02c6cbcd63d13edf2d179125a8417730cb2d750
mikeylinton <79585896+mikeylinton@users.noreply.github.com>
2021-08-18 16:33:28 +0100

    Update apacheSecurity.sh

--- apacheSecurity.sh
+++ apacheSecurity.sh
@@ -7,8 +7,8 @@ printf "\nServerTokens Prod\nServerSignature  Off\n" >> /etc/apache2/conf-enable
 printf "<Directory /var/www/>\n\tAllowOverride None\n\tRequire all denied\n</Directory>\n" >> /etc/apache2/conf-enabled/security.conf
 printf "<Directorymatch \"^/.*/\.git/\">\n\tOrder 'deny,allow'\n\tDeny from all\n</Directorymatch>\n" >> /etc/apache2/conf-enabled/security.conf
 printf "<Files ~ \"^\.git\">\n\tOrder 'deny,allow'\n\tDeny from all\n</Files>\n" >> /etc/apache2/conf-enabled/security.conf
-mkdir /etc/apache2/ssl/
 printf "\nServerName localhost\n" >> /etc/apache2/apache2.conf
+mkdir /etc/apache2/ssl/
 cd /etc/php/7.3/apache2/
 find . -name 'php.ini' -exec sed -i -e 's/expose_php = Off/expose_php = On/g' {} \;
 systemctl reload apache2
