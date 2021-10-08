e59fa4cee0fc75218f739ca853561cd3f4db00a0
Michael (Mikey) Linton <michaeljlinton.ml@gmail.com>
2021-10-08 16:07:16 +0100

    Update apacheSecurity.sh

--- bash/apacheSecurity.sh
+++ bash/apacheSecurity.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 if [[ $(/usr/bin/id -u) -ne 0 ]]; then
         echo "This script must be run as root"
@@ -6,18 +7,19 @@ fi
 PHP_Version=$(php -v | grep -o "PHP "[0-9].[0-9] | grep -o [0-9].[0-9])
 Apache2_Directory="/etc/apache2"
 cd $Apache2_Directory
-printf "\nServerTokens Prod\nServerSignature  Off\n" >> conf-enabled/security.conf
+printf "\nHeader always set X-Content-Type-Options nosniff\nHeader always edit Set-Cookie ^(.*)$ $1;HttpOnly;Secure\nHeader always set X-Frame-Options SAMEORIGIN\nHeader always set X-XSS-Protection \"1; mode=block\"\nHeader always set Strict-Transport-Security \"max-age=31536000; includeSubdomains\"\n" >> conf-enabled/security.conf
+printf "\nServerTokens Prod\nServerSignature  Off\nTraceEnable Off\n" >> conf-enabled/security.conf
 printf "<Directory /var/www/>\n\tAllowOverride None\n\tRequire all denied\n</Directory>\n" >> conf-enabled/security.conf
-printf "<Directorymatch \"^/.*/\.git/\">\n\tOrder 'deny,allow'\n\tDeny from all\n</Directorymatch>\n" >> conf-enabled/security.conf
-printf "<Files ~ \"^\.git\">\n\tOrder 'deny,allow'\n\tDeny from all\n</Files>\n" >> conf-enabled/security.conf
+#printf "<Directorymatch \"^/.*/\.git/\">\n\tOrder 'deny,allow'\n\tDeny from all\n</Directorymatch>\n" >> conf-enabled/security.conf
+#printf "<Files ~ \"^\.git\">\n\tOrder 'deny,allow'\n\tDeny from all\n</Files>\n" >> conf-enabled/security.conf
 #\n<LimitExcept GET POST HEAD>\n\tdeny from all\n</LimitExcept>
 printf "\n<LocationMatch \"\\/\\.\\\">\n\tRequire all denied\n</LocationMatch>\nFileETag None\nTraceEnable off\nServerName localhost\n" >> apache2.conf
-find . -name 'apache2.conf' -exec sed -i -e 's/<Directory \/var\/www\/>/<Directory \/var\/www\/>\n\t<IfModule mod_headers.c>\n\t\tHeader always set X-Content-Type-Options nosniff\n\t\tHeader edit Set-Cookie ^(.*)$ $1;HttpOnly;Secure\n\t\tHeader always set X-Frame-Options SAMEORIGIN\n\t\tHeader always set X-XSS-Protection \"1; mode=block\"\n\t\tHeader always set Strict-Transport-Security \"max-age=31536000; includeSubdomains\"\n\t<\/IfModule>\n\t<IfModule mod_rewrite.c>\n\t\tRewriteEngine On\n\t\tRewriteCond %{THE_REQUEST} !HTTP\/1.1$\n\t\tRewriteRule .* - [F]\n\t<\/IfModule>/g' {} \;
+#find . -name 'apache2.conf' -exec sed -i -e 's/<Directory \/var\/www\/>/<Directory \/var\/www\/>\n\t<IfModule mod_headers.c>\n\t\tHeader always set X-Content-Type-Options nosniff\n\t\tHeader edit Set-Cookie ^(.*)$ $1;HttpOnly;Secure\n\t\tHeader always set X-Frame-Options SAMEORIGIN\n\t\tHeader always set X-XSS-Protection \"1; mode=block\"\n\t\tHeader always set Strict-Transport-Security \"max-age=31536000; includeSubdomains\"\n\t<\/IfModule>\n\t<IfModule mod_rewrite.c>\n\t\tRewriteEngine On\n\t\tRewriteCond %{THE_REQUEST} !HTTP\/1.1$\n\t\tRewriteRule .* - [F]\n\t<\/IfModule>/g' {} \;
 find . -name 'apache2.conf' -exec sed -i -e 's/Timeout 300/Timeout 60/g' {} \;
-find . -name 'apache2.conf' -exec sed -i -e 's/Options FollowSymLinks/Options -Indexes/g' {} \;
-find . -name 'apache2.conf' -exec sed -i -e 's/FollowSymLinks//g' {} \;
-find . -name 'apache2.conf' -exec sed -i -e 's/Options Indexes/Options -Indexes/g' {} \;
-find . -name 'apache2.conf' -exec sed -i -e 's/Require all granted/Require all denied/g' {} \;
+#find . -name 'apache2.conf' -exec sed -i -e 's/Options FollowSymLinks/Options -Indexes/g' {} \;
+#find . -name 'apache2.conf' -exec sed -i -e 's/FollowSymLinks//g' {} \;
+#find . -name 'apache2.conf' -exec sed -i -e 's/Options Indexes/Options -Indexes/g' {} \;
+#find . -name 'apache2.conf' -exec sed -i -e 's/Require all granted/Require all denied/g' {} \;
 cd /etc/php/$PHP_Version/apache2/
 find . -name 'php.ini' -exec sed -i -e 's/expose_php = On/expose_php = Off/g' {} \;
 mkdir $Apache2_Directory/ssl/
